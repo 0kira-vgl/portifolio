c1a860170c2bf4a784efc15a846b29ff8986b0a4
Matheus Henrique <matheusgtiburcio@gmail.com>
2024-01-17 13:03:34 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div>
+      <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Hello Word!
+      </h1>
+    </div>
+  );
+}
