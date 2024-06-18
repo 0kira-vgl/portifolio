7884e4c99494d068c2425dd4c9dd014e3c76263b
Matheus Henrique <matheusgtiburcio@gmail.com>
2024-06-17 22:02:12 -0300

    add project cards

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,8 +1,6 @@
-import "./index.css";
-
 export function Projects() {
   return (
-    <section className="container pt-32">
+    <section className="container mx-auto w-full max-w-7xl p-6 pt-32">
       <div className="items-center justify-center pb-5">
         <h2 className="relative z-10 pb-2 text-center text-4xl font-extrabold text-[#1E293B]">
           Projetos
@@ -10,6 +8,191 @@ export function Projects() {
         <p className="mx-auto max-w-[17rem] text-center text-sm">
           Veja os projetos onde coloquei meus conhecimentos em prática.
         </p>
+      </div>
+
+      <div className="mb-8 grid grid-cols-2 gap-12">
+        <div>
+          <a
+            href="#"
+            className="relative flex h-full flex-col overflow-hidden rounded-xl shadow-lg outline-accent transition-transform duration-500 hover:scale-[1.03]"
+          >
+            <div className="border-zinc-300relative relative z-20 aspect-video w-full overflow-hidden rounded-lg border-b-[3px] border-zinc-300">
+              <img
+                src="https://github.com/0kira-vgl/nlwNotes/raw/main/src/assets/thumbnail.png?raw=true"
+                alt=""
+              />
+            </div>
+            <div className="flex grow flex-col p-3">
+              <h2 className="text-lg font-bold text-slate-700">
+                Explorer Notes
+              </h2>
+              <p className="mb-3 text-sm text-neutral-500">
+                Compre e venda o que quiser, pelo preço que quiser!
+              </p>
+              <div className="mt-auto flex justify-between gap-3">
+                <div className="flex items-center gap-2">
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+                </div>
+                <a
+                  href="#"
+                  className="inline-block rounded-full border border-slate-700 px-8 py-1 text-sm text-slate-700 shadow-md outline-accent"
+                >
+                  Ver mais
+                </a>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="#"
+            className="relative flex h-full flex-col overflow-hidden rounded-xl shadow-lg outline-accent transition-transform duration-500 hover:scale-[1.03]"
+          >
+            <div className="border-zinc-300relative relative z-20 aspect-video w-full overflow-hidden rounded-lg border-b-[3px] border-zinc-300">
+              <img
+                src="https://github.com/0kira-vgl/nlwNotes/raw/main/src/assets/thumbnail.png?raw=true"
+                alt=""
+              />
+            </div>
+            <div className="flex grow flex-col p-3">
+              <h2 className="text-lg font-bold text-slate-700">
+                Explorer Notes
+              </h2>
+              <p className="mb-3 text-sm text-neutral-500">
+                Compre e venda o que quiser, pelo preço que quiser!
+              </p>
+              <div className="mt-auto flex justify-between gap-3">
+                <div className="flex items-center gap-2">
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+                </div>
+                <a
+                  href="#"
+                  className="inline-block rounded-full border border-slate-700 px-8 py-1 text-sm text-slate-700 shadow-md outline-accent"
+                >
+                  Ver mais
+                </a>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 gap-8 align-super">
+        <div>
+          <a
+            href=""
+            className="relative flex h-full flex-col overflow-hidden rounded-xl shadow-lg outline-accent transition-transform duration-500 hover:scale-[1.03]"
+          >
+            <div className="relative p-2">
+              <div className="relative z-20 aspect-video w-full overflow-hidden rounded-lg border-b-[3px] border-zinc-300">
+                <img
+                  src="https://github.com/0kira-vgl/nlwNotes/raw/main/src/assets/thumbnail.png?raw=true"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="flex grow flex-col p-3">
+              <h2 className="text-lg font-bold text-slate-700">
+                Explorer Notes
+              </h2>
+              <p className="mb-3 text-sm text-neutral-500">
+                Rede social criada com NodeJS, Express e MySQL.
+              </p>
+              <div className="mt-auto flex justify-between gap-3">
+                <div className="flex items-center gap-2">
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+                </div>
+                <a
+                  href=""
+                  className="inline-block rounded-full border border-slate-700 px-8 py-1 text-sm text-slate-700 shadow-md outline-accent"
+                >
+                  Ver mais
+                </a>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div>
+          <a
+            href=""
+            className="relative flex h-full flex-col overflow-hidden rounded-xl shadow-lg outline-accent transition-transform duration-500 hover:scale-[1.03]"
+          >
+            <div className="relative p-2">
+              <div className="relative z-20 aspect-video w-full overflow-hidden rounded-lg border-b-[3px] border-zinc-300">
+                <img
+                  src="https://github.com/0kira-vgl/nlwNotes/raw/main/src/assets/thumbnail.png?raw=true"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="flex grow flex-col p-3">
+              <h2 className="text-lg font-bold text-slate-700">
+                Explorer Notes
+              </h2>
+              <p className="mb-3 text-sm text-neutral-500">
+                Rede social criada com NodeJS, Express e MySQL.
+              </p>
+              <div className="mt-auto flex justify-between gap-3">
+                <div className="flex items-center gap-2">
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+                </div>
+                <a
+                  href=""
+                  className="inline-block rounded-full border border-slate-700 px-8 py-1 text-sm text-slate-700 shadow-md outline-accent"
+                >
+                  Ver mais
+                </a>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div>
+          <a
+            href=""
+            className="relative flex h-full flex-col overflow-hidden rounded-xl shadow-lg outline-accent transition-transform duration-500 hover:scale-[1.03]"
+          >
+            <div className="relative p-2">
+              <div className="relative z-20 aspect-video w-full overflow-hidden rounded-lg border-b-[3px] border-zinc-300">
+                <img
+                  src="https://github.com/0kira-vgl/nlwNotes/raw/main/src/assets/thumbnail.png?raw=true"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="flex grow flex-col p-3">
+              <h2 className="text-lg font-bold text-slate-700">
+                Explorer Notes
+              </h2>
+              <p className="mb-3 text-sm text-neutral-500">
+                Rede social criada com NodeJS, Express e MySQL.
+              </p>
+              <div className="mt-auto flex justify-between gap-3">
+                <div className="flex items-center gap-2">
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
+                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+                </div>
+                <a
+                  href=""
+                  className="inline-block rounded-full border border-slate-700 px-8 py-1 text-sm text-slate-700 shadow-md outline-accent"
+                >
+                  Ver mais
+                </a>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
     </section>
   );
